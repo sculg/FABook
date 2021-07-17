bfee906343845ed34ecf859bf32488d029ece7ab
FAB <sculisong@163.com>
2021-07-17 22:54:17 +0800

    增加pch文件

--- FABook/Pods/Target Support Files/Pods-FABook/Pods-FABook-frameworks.sh
+++ FABook/Pods/Target Support Files/Pods-FABook/Pods-FABook-frameworks.sh
@@ -175,20 +175,30 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CTMediator/CTMediator.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CYLTabBarController/CYLTabBarController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FABAllTreasure/FABAllTreasure.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FABCoreSDK/FABCoreSDK.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FABDataCenter/FABDataCenter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LKDBHelper/LKDBHelper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CTMediator/CTMediator.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CYLTabBarController/CYLTabBarController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FABAllTreasure/FABAllTreasure.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FABCoreSDK/FABCoreSDK.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FABDataCenter/FABDataCenter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LKDBHelper/LKDBHelper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
